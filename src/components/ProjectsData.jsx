51036c58ffa1edf12f108420253c8a226d7f716c
Gisela84spina <93849607+Gisela84spina@users.noreply.github.com>
2025-05-05 17:11:36 -0300

    fix: actualizo con imágenes gratuitas

--- src/components/ProjectsData.jsx
+++ src/components/ProjectsData.jsx
@@ -1,15 +1,14 @@
 const projectsData = [
-    {
-      title: 'E-commerce (Tienda Online)',
-      description: 'Un proyecto de tienda online donde los usuarios pueden comprar productos.',
-      link: '#', // Aquí iría el enlace al proyecto
-      imageUrl: 'https://via.placeholder.com/400x200?text=E-commerce', // Imagen de ejemplo
-    },
-    {
-      title: 'Próximo Proyecto',
-      description: 'Descripción del próximo proyecto que quiero crear.',
-      link: '#', // Aquí iría el enlace al proyecto
-      imageUrl: 'https://via.placeholder.com/400x200?text=Próximo+Proyecto', // Imagen de ejemplo
-    },
-  ];
-  
+  {
+    title: 'E-commerce (Tienda Online)',
+    description: 'Un proyecto de tienda online donde los usuarios pueden comprar productos.',
+    link: '#',
+    imageUrl: 'https://images.pexels.com/photos/922203/pexels-photo-922203.jpeg', // Imagen E-commerce
+  },
+  {
+    title: 'Próximo Proyecto',
+    description: 'Descripción del próximo proyecto que quiero crear.',
+    link: '#',
+    imageUrl: 'https://images.pexels.com/photos/1108095/pexels-photo-1108095.jpeg', // Imagen Proyecto futuro
+  },
+];
