6e7615826e9d5eb1b4c9859654817f01950aaac4
Gisela84spina <93849607+Gisela84spina@users.noreply.github.com>
2026-03-24 19:33:46 -0300

    cambie download por target del cv

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,7 +33,8 @@ export default function Header() {
           <li>
             <a
               href="/cv.pdf"
-              download
+              target='_blank'
+              rel='noopener noreferrer'
               className="flex items-center gap-2 px-3 py-1.5 rounded-md bg-red-500 text-white hover:bg-red-600 transition"
             >
               <FileText size={18} />
